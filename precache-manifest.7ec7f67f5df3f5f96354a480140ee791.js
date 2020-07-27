be1895a7724b6bc08a93dc609058f8980ceb875c
willmharris <willmharris0@gmail.com>
2020-07-27 18:26:02 -0400

    Updates

---
+++ precache-manifest.7ec7f67f5df3f5f96354a480140ee791.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deed61b49fd4839f1866521e3115d579",
+    "url": "/tweet-mapper/index.html"
+  },
+  {
+    "revision": "4cbb3df14a33eecbb467",
+    "url": "/tweet-mapper/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "121e63ab9b778654ab9b",
+    "url": "/tweet-mapper/static/js/2.8cea0b6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tweet-mapper/static/js/2.8cea0b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cbb3df14a33eecbb467",
+    "url": "/tweet-mapper/static/js/main.921f28fb.chunk.js"
+  },
+  {
+    "revision": "07eba16d964e19994f53",
+    "url": "/tweet-mapper/static/js/runtime-main.ff6d8d05.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tweet-mapper/static/media/logo.5d5d9eef.svg"
+  }
+]);
